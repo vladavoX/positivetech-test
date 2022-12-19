510d10c7c693da87ed9de663338ab945324f477f
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-12-19 12:20:53 +0100

    App ready

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,11 @@
+import { AiOutlineSearch } from 'react-icons/ai'
+import Image from 'next/image'
+
 import styles from '../styles/Input.module.css'
+import weatherIcon from '../public/weather.svg'
+import rs from '../public/rs.svg'
+import gb from '../public/uk.svg'
+import nl from '../public/nl.svg'
 
 interface Props {
   fetchWeather: () => void
@@ -25,12 +32,18 @@ const Input = ({ fetchWeather, setLoading, setLocation }: Props) => {
 
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
-      <select name='country' onChange={handleChange}>
-        <option value="GB">GB</option>
-        <option value="RS">RS</option>
-        <option value="NL">NL</option>
-      </select>
-      <input type="text" name='city' placeholder='Please enter your location...' onChange={handleChange} />
+      <Image src={weatherIcon} width={25} height={25} alt='weather icon' />
+      <div className={styles.custom_select}>
+        <select name='country' onChange={handleChange}>
+          <option value="GB">GB</option>
+          <option value="RS">RS</option>
+          <option value="NL">NL</option>
+        </select>
+      </div>
+      <div className={styles.input_container}>
+        <input type="text" name='city' placeholder='Please enter your location...' onChange={handleChange} />
+        <AiOutlineSearch className={styles.icon} />
+      </div>
     </form>
   )
 }
